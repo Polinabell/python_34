d5be818264e258039b524ce9fb86775ddb4fd3c2
belka <belkapoli@gmail.com>
2022-10-08 15:10:37 +0300

    Home Work

---
+++ hm_2.1.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+s = a + b
+print('Сумма чисел:', str(a), '+', str(b), '=', s)
+print('Произведение чисел:', str(a), '*', str(b), '=', a * b)
+print('Разность чисел:', str(a), '/', str(b), '=', a / b)
